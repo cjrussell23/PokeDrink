9607d8f981419ac788fb1326829475a4626528cf
cjrussell23 <cj.russell2396@gmail.com>
2022-08-01 00:38:38 -0600

    Added tile movement and a dice

--- PokeDrink/Assets/Mirror/Examples/Benchmark/Scripts/PlayerMovement.cs
+++ PokeDrink/Assets/Mirror/Examples/Benchmark/Scripts/PlayerMovement.cs
@@ -5,46 +5,7 @@ namespace Mirror.Examples.Benchmark
 {
     public class PlayerMovement : NetworkBehaviour
     {
-        public float speed = 20;
-        public GameObject PlayerModel;
-        public SpriteRenderer sprite;
-
-        public void Start()
-        {
-            sprite = PlayerModel.GetComponent<SpriteRenderer>();
-            sprite.enabled = false;
-            transform.position = new Vector3(-23, -13, 0);
-        }
-
-        void Update()
-        {
-            SceneManager.activeSceneChanged += SceneChanged;
-
-            if (hasAuthority)
-                {
-                    Movement();
-                }
-        }
-        public void SceneChanged(Scene current, Scene next)
-        {
-            Debug.Log("SceneChanged");
-            if (SceneManager.GetActiveScene().name.Equals("Scene_SteamworksGame"))
-            {
-                sprite.enabled = true;
-            }
-        }
-
-        public void Movement()
-        {
-            if (!isLocalPlayer)
-                return;
-
-            float h = Input.GetAxis("Horizontal");
-            float v = Input.GetAxis("Vertical");
-
-            Vector3 dir = new Vector3(h, v, 0);
-            transform.position += dir.normalized * (Time.deltaTime * speed);
-        }
+        
     }
 
 }
---
+++ PokeDrink/Assets/scripts/GridMovement.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.SceneManagement;
+
+public class GridMovement : NetworkBehaviour
+{
+    private bool isMoveing;
+    private Vector3 origPos;
+    private Vector3 targetPos;
+    [SerializeField] private LayerMask stopMovement;
+
+    [SerializeField]
+    private float timeToMove = 0.2f;
+    public GameObject PlayerModel;
+    public SpriteRenderer sprite;
+
+    public void Start()
+    {
+        sprite = PlayerModel.GetComponent<SpriteRenderer>();
+        sprite.enabled = false;
+        transform.position = new Vector3(-24.5f, -12.35f, 0);
+    }
+
+    void Update()
+    {
+        SceneManager.activeSceneChanged += SceneChanged;
+
+        if (hasAuthority)
+        {
+            Movement();
+        }
+    }
+
+    public void SceneChanged(Scene current, Scene next)
+    {
+        Debug.Log("SceneChanged");
+        if (SceneManager.GetActiveScene().name.Equals("Scene_SteamworksGame"))
+        {
+            sprite.enabled = true;
+        }
+    }
+
+    public void Movement()
+    {
+        if (!isLocalPlayer)
+            return;
+        if (!isMoveing)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                StartCoroutine(MovePlayer(Vector3.up));
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                StartCoroutine(MovePlayer(Vector3.down));
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                StartCoroutine(MovePlayer(Vector3.left));
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                StartCoroutine(MovePlayer(Vector3.right));
+            }
+        }
+    }
+
+    public IEnumerator MovePlayer(Vector3 dir)
+    {
+        isMoveing = true;
+        float elapsedTime = 0;
+        origPos = transform.position;
+        targetPos = origPos + dir;
+        // Check if the target position is valid
+        if (Physics2D.OverlapCircle(targetPos, 0.2f, stopMovement))
+        {
+            targetPos = origPos;
+        }
+
+        while (elapsedTime < timeToMove)
+        {
+            transform.position = Vector3.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPos;
+        isMoveing = false;
+    }
+}
